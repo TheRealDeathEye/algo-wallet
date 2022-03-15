b205113545499ea3a01dd5702085581512d3245f
TheRealDeathEye <therealdeatheye@gmail.com>
2022-03-15 02:38:57 -0500

    logoAdded

---
+++ src/components/home.jsx
@@ -0,0 +1,10 @@
+import logo from '../imgs/logo.png';
+
+export default function Home() {
+    return(
+        <div>
+            <br/>
+            <img style={{width:'200px'}} src={logo} alt='' />
+        </div>
+    );
+}
