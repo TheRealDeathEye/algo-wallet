0f91853dbcd3e03f0613790f6c0b373b1e50f7e7
TheRealDeathEye <therealdeatheye@gmail.com>
2022-03-15 03:43:00 -0500

    buttonsAdded

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import {Routes,Route} from 'react-router-dom';
 import Home from './components/home';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   document.body.style = 'background: black;'
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,10 +1,42 @@
 import logo from '../imgs/logo.png';
+import receive from '../imgs/receive.png';
+import send from '../imgs/send.png';
+import connect from '../imgs/connect.png';
+import Card from 'react-bootstrap/Card';
 
 export default function Home() {
+    const total = 400;
+
     return(
         <div>
-            <br/>
-            <img style={{width:'200px'}} src={logo} alt='' />
+            <div align='center'>
+                <br/>
+                <img style={{width:'200px'}} src={logo} alt='' />
+            </div>
+            <div align='center' style={{marginTop:'100px'}}>
+                <h1>{total} Algo</h1>
+                <h5 style={{paddingTop:'10px'}}>~ $400</h5>
+                <div className='row' style={{paddingTop:'40px', maxWidth:'350px'}}>
+                    <div className='col'>
+                    <Card style={{backgroundColor:'transparent'}}>
+                        <Card.Img variant='top' src={receive} />
+                        <Card.Text>Receive</Card.Text>
+                    </Card>
+                    </div>
+                    <div className='col'>
+                    <Card style={{backgroundColor:'transparent'}}>
+                        <Card.Img variant='top' src={send} />
+                        <Card.Text>Send</Card.Text>
+                    </Card>
+                    </div>
+                    <div className='col'>
+                    <Card style={{backgroundColor:'transparent'}}>
+                        <Card.Img variant='top' src={connect} />
+                        <Card.Text>Connect</Card.Text>
+                    </Card>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
