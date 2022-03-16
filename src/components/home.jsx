3665855b3d1bbaaa42ca41494597d3ab7460a1e6
paul fears <paulrfears@gmail.com>
2022-03-16 05:04:26 -0500

    added recive qr code

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,15 +1,19 @@
 import logo from '../imgs/logo.png';
-import receive from '../imgs/receive.png';
+import copyIcon from '../imgs/copyIcon.png';
+import receiveIcon from '../imgs/receive.png';
 import send from '../imgs/send.png';
 import connect from '../imgs/connect.png';
 import Card from 'react-bootstrap/Card';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 import { useState } from 'react';
+import AddressCard from './AddressCard';
 
 export default function Home() {
     const total = 400;
     const [connected, set_connected] = useState(false);
     const [address, set_address] = useState('');
     const [balance, set_balance] = useState('');
+    const [receive, set_receive] = useState('');
     const connect_func = async () => {
         await window.ethereum.request({
             method: 'wallet_enable',
@@ -42,20 +46,32 @@ export default function Home() {
           })
         set_address(address);
       }
-
+    const toggleReceive = () => {
+        set_receive(!receive);
+    }
     return(
         <div>
             <div align='center'>
                 <br/>
                 <img style={{width:'200px'}} src={logo} alt='' />
-                <p>{address}</p>
+                
             </div>
-            <div align='center' style={{marginTop:'100px'}}>
+            <div align='center' style={{marginTop:'60px'}}>
                 
-                <h1>{balance} Algo</h1>
+                <h1>{balance} ALGO</h1>
+                {connected ?
+                <div style={{display:'flex', justifyContent: 'center'}}>
+                  <p style={{fontSize: '75%', marginRight:'5px'}}>{address}</p>
+                  <CopyToClipboard text={address}>
+                  <img style={{width: '20px', height: '20px', cursor:'pointer'}} src={copyIcon}/>
+                  </CopyToClipboard>
+                </div>
+                :
+                null
+                }
                 
-                <h5 style={{paddingTop:'10px'}}>~ $400</h5>
-                <div className='row' style={{paddingTop:'40px', maxWidth:'350px'}}>
+                
+                <div className='row' style={{paddingTop:'20px', maxWidth:'350px'}}>
                     <div className='col'>
                     <Card onClick={connect_func} style={{backgroundColor:'transparent'}}>
                         <Card.Img variant='top' src={connect} />
@@ -64,8 +80,8 @@ export default function Home() {
                     </div>
                     <div className='col'>
                     <Card style={{backgroundColor:'transparent'}}>
-                        <Card.Img variant='top' src={receive} />
-                        <Card.Text>Receive</Card.Text>
+                        <Card.Img onClick={toggleReceive} variant='top' src={receiveIcon} />
+                        <Card.Text onClick={toggleReceive}>Receive</Card.Text>
                     </Card>
                     </div>
                     <div className='col'>
@@ -75,6 +91,14 @@ export default function Home() {
                     </Card>
                     </div>
                 </div>
+                <br/>
+                {receive?
+                  <div>
+                    {connected?<AddressCard address={address}/>:<p>connect wallet first</p>}
+                  </div>
+                        :
+                        null
+                        }
             </div>
         </div>
     );
